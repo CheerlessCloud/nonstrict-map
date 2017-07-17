31524cf41d42b63ee6f2bf905656ff7fe02ef624
EnRoute <nnsceg@gmail.com>
2017-07-17 08:17:25 +0300

    chore(map): Add prototype of code and ToDo tests

---
+++ src/transforms.js
@@ -0,0 +1,16 @@
+module.exports = {
+  /**
+   * @param {any} key
+   * @return {(string|number|boolean|NaN)}
+   */
+  ifTransform(key) {
+
+  },
+  /**
+   * @param {(string|number|boolean|NaN)} key
+   * @return {any}
+   */
+  outTransform(key) {
+
+  }
+}
---
+++ test/transforms.test.js
@@ -0,0 +1,5 @@
+const test = require('ava');
+const transforms = require('./../src/transforms');
+
+test.todo('in transform key');
+test.todo('out transform key');
