98b10051f0c84c4d80fb8683c857b07e1fedad57
EnRoute <nnsceg@gmail.com>
2017-07-17 08:54:34 +0300

    feat(transforms): Add JSON as default serialize and desialize format and tests for in/out functions.

--- src/transforms.js
+++ src/transforms.js
@@ -1,16 +1,27 @@
+/**
+ * @typedef {function(key: any).<(string|number|boolean|NaN)>} inTransformFunction
+ */
+/**
+ * @typedef {function(key: (string|number|boolean)).<any>} outTransformFunction
+ */
+
 module.exports = {
   /**
    * @param {any} key
-   * @return {(string|number|boolean|NaN)}
+   * @return {(string|number|boolean)}
    */
-  ifTransform(key) {
-
+  in(key) {
+    return JSON.stringify(key);
   },
   /**
-   * @param {(string|number|boolean|NaN)} key
+   * @param {(string|number|boolean)} key
    * @return {any}
    */
-  outTransform(key) {
+  out(key) {
+    return JSON.parse(key);
+  },
+};
 
-  }
-}
+// Yes, JSON is not a good format for the keys, but it prototype...
+// In future I replace JSON with more compact and quick format.
+// ToDo: find good format for keys serializing
--- test/transforms.test.js
+++ test/transforms.test.js
@@ -1,5 +1,41 @@
 const test = require('ava');
 const transforms = require('./../src/transforms');
 
-test.todo('in transform key');
-test.todo('out transform key');
+function testTransform(t, keys, message) {
+  for (const key of keys) {
+    t.notThrows(() => {
+      const outKey = transforms.out(transforms.in(key));
+      t.deepEqual(key, outKey, message);
+    });
+  }
+}
+
+test('correct serialize and deserialize object key', (t) => {
+  const keys = [
+    { x: 5, y: 7 },
+    { foo: 'bar' },
+  ];
+
+  testTransform(t, keys, 'keys must be equal on simple objects');
+});
+
+test('correct serialize and deserialize array key', (t) => {
+  const keys = [
+    ['foo', 'bar'],
+    [['foo'], [5636]],
+  ];
+
+  testTransform(t, keys, 'keys must be equal on arrays');
+});
+
+test('correct serialize and deserialize primitives keys', (t) => {
+  const keys = [
+    'foo\u2B20',
+    5688123,
+    (10 / 3),
+    null,
+    // NaN // yes, in JSON NaN cast to null. Fix it.
+  ];
+
+  testTransform(t, keys, 'keys must be equal on primitives');
+});
